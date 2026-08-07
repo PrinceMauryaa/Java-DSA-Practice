fcd11e9af564b8527ec4031ba8bd8a29917e442b
Prince Maurya <prince.24vns@gmail.com>
2026-08-07 09:48:35 +0530

    java Basic Practice

---
+++ Day01.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Day01 {
+
+    public static void main(String args[]){
+
+
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a*b;
+        // System.out.println(product);
+        // int radius = sc.nextInt();
+        // float pie = 3.14f ;
+        // float area = pie * radius * radius;
+        // System.out.println(area);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+
+        // float avg = (a+b+c)/3;
+        // System.out.println(avg);
+
+        // int side = sc.nextInt();
+        // int area = side*side;
+        // System.out.println(area);
+
+
+        // float pencil = sc.nextInt();
+        // float pen = sc.nextInt();
+        // float eraser = sc.nextInt();
+        // float sum = (pencil+pen+eraser);
+        // float gst = ((pencil+pen+eraser)/100)*18;
+        // float incld = sum+gst;
+
+        // System.out.println("Total bill : "+ sum);
+        // System.out.println("Total bill with GST 18 % : "+ incld);
+        System.out.println("Hello World");
+
+
+    }
+}
