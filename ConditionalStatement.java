77c14b417ed45344598e33f0d255721c318c1ef4
Prince Maurya <prince.24vns@gmail.com>
2026-08-07 13:03:22 +0530

    if - else java practice

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -3,11 +3,36 @@ import java.util.Scanner;
 public class ConditionalStatement {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
-        Scanner sco = new Scanner(System.in);
-        int a = sco.nextInt();
-        System.out.println("HEllooo " + a);
-        System.out.println("This is Your Answer");
+       // Print the largest of 2 numbers
+
+    //    int num1 = 45;
+    //    int num2 = 98;
+
+    //    if (num1>num2){
+    //     System.out.println(num1);
+    //    }
+    //    else{
+    //     System.out.println(num2);
+    //    }
+
+      // Print if a number is odd number
+
+      System.out.println("Enter The number :");
+      int newNum = scan.nextInt();
+
+      if(newNum%2==0){
+        System.out.println("Number is Even !!");
+    
+      }
+      else{
+        System.out.println("Number is Odd !!");
+      }
+         
+
+    
+    
 
     }
 }
