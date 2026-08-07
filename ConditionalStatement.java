7a45a654a19ec2b413a5301866e870dd0af8876c
Prince Maurya <prince.24vns@gmail.com>
2026-08-07 09:54:04 +0530

    java contional statement

---
+++ ConditionalStatement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        
+        Scanner sco = new Scanner(System.in);
+        int a = sco.nextInt();
+        System.out.println("HEllo " + a);
+
+    }
+}
