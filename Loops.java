e88cb282b59412aad91ec049ab93f10533e2f3e4
Prince Maurya <prince.24vns@gmail.com>
2026-08-09 10:19:51 +0530

    loop practice start

---
+++ Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+        int count = 01 ;
+
+        // while (count<100) {
+        //     System.out.println("Hello"+count);
+        //     count++;
+            
+        // }
+
+        // Count 1-10
+
+        while (count <= 10) {
+            System.out.println("no. :"+count);
+            count++;
+        }
+
+    }
+    
+}
