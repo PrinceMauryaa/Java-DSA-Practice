53545e18335ebacfd3fdae216b044e8b023856b5
Prince Maurya <prince.24vns@gmail.com>
2026-08-09 11:54:50 +0530

    break and continue

--- Loops.java
+++ Loops.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
-        int count = 01 ;
+        Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter the number : ");
+        // int rang = scan.nextInt();
+
+        // int count = 01 ;
 
         // while (count<100) {
         //     System.out.println("Hello"+count);
@@ -10,11 +16,91 @@ public class Loops {
 
         // Count 1-10
 
-        while (count <= 10) {
-            System.out.println("no. :"+count);
-            count++;
-        }
+        // while (count <= rang) {
+        //     System.out.println("no. :"+count);
+        //     count++;
+        // }
 
+
+
+        // sum of first n numbers 
+
+        // int res = 0;
+        // while (count<rang) {
+        //     res += count;
+        //     count++;
+        // }
+
+        // System.out.println("Result is :"+res);
+
+
+        // For Loop 
+
+        // for(int i = 1 ; i<=10 ; i++){
+        //     System.out.println("Hello Bro : "+i);
+        // }
+
+        // Printing square 
+
+        // for(int j = 1 ; j<=10 ; j++){
+        //     System.out.println("");
+        // for(int i = 1 ; i<=10 ; i++){
+        //     System.out.print("*");
+        // }}
+
+
+        // Flip an Number 
+
+        // int num = 12345;
+        // int lstNum = 0;
+        // int newnum = 0;
+        // while (num > 0) {
+        //     lstNum = num % 10 ;
+        //     newnum = (newnum *10) + lstNum;
+        //     num = num /10;
+        // }
+        // System.out.print(newnum);
+
+
+        // Break Statement 
+
+        // do{
+        //     System.out.println("Enter the number : ");
+        //     int num = scan.nextInt();
+        //     if (num % 10 == 0) {
+        //         System.out.println("end Ho gya");
+        //         break;
+        //     }
+        // }while(true);
+
+
+        // Continue 
+        // for(int i = 0 ; i< 100 ; i ++){
+        //     if (i %10 == 0){
+        //         continue;
+        //     }
+        //     System.out.println("Your number : "+i);
+        // }
+
+
+        // Prime Numberr
+        System.out.println("Enter number :");
+        int num = scan.nextInt();
+        boolean isPrime = true;
+
+        for (int i = 2 ; i <= num/2 ; i++){
+            if (num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime == false){
+            System.out.println("This is not Prime !");
+        }
+        else{
+            System.out.println("This Is  Prime");
+        }
     }
     
 }
