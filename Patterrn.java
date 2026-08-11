06ffab81b5388e932c7ca41b640b38e5cfbb304f
Prince Maurya <Prince.24vns@gmail.com>
2026-08-12 02:32:05 +0530

    Making pattern Triangles

---
+++ Patterrn.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Patterrn {
+
+    public static void main (String abc[]){
+
+        // first Pattern
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Ente The Size Of triangle :: ");
+        int size = scan.nextInt();
+
+        for(int i = 1 ; i <= size ; i++){
+            for(int j = i ; j > 0 ; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
+        // inveted Triangle !!
+
+
+
+        for(int i = 1 ; i<= size ; i++){
+            for(int j = size ; j > i ; j-- ){
+
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
