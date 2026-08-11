bc6940e4ec11483e610b849e0d1bfcd284a2adea
Prince Maurya <Prince.24vns@gmail.com>
2026-08-12 02:38:27 +0530

    Half Piramid question

--- Patterrn.java
+++ Patterrn.java
@@ -9,25 +9,36 @@ public class Patterrn {
         System.out.print("Ente The Size Of triangle :: ");
         int size = scan.nextInt();
 
-        for(int i = 1 ; i <= size ; i++){
-            for(int j = i ; j > 0 ; j--){
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
+        // for(int i = 1 ; i <= size ; i++){
+        //     for(int j = i ; j > 0 ; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
 
 
         // inveted Triangle !!
 
 
 
-        for(int i = 1 ; i<= size ; i++){
-            for(int j = size ; j > i ; j-- ){
+        // for(int i = 1 ; i<= size ; i++){
+        //     for(int j = size ; j > i ; j-- ){
 
-                System.out.print("*");
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+
+        // }
+
+
+
+        // Half Piramid 
+
+        for(int i = 1; i<=size;i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print(j);
             }
             System.out.println("");
-
         }
 
     }
