09fddf8127d4a98e75be00503f60cc561a596e88
Prince Maurya <Prince.24vns@gmail.com>
2026-08-16 03:10:19 +0530

    Inverted Half pirammid with number !!

--- PatternPart2.java
+++ PatternPart2.java
@@ -38,12 +38,22 @@ public class PatternPart2 {
 
     }
 
+    public static void InvPiraWithNum(int num ){
+        for(int i = num; i >= 1 ; i-- ){
+            for (int j = 1 ; j <= i ; j++ ){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+
     
 
 
     public static void main(String[] args) {
 
         // HollowBox(5);
-        InvTrangle(8);
+        // InvTrangle(8);
+        InvPiraWithNum(5);
     }
 }
