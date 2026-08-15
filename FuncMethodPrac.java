d4de7dbab066b97f7e29360d55fc6fbb793e90ce
Prince Maurya <Prince.24vns@gmail.com>
2026-08-16 01:32:07 +0530

    Paratice FuncMethod !!

---
+++ FuncMethodPrac.java
@@ -0,0 +1,25 @@
+public class FuncMethodPrac {
+    
+    public static int Avg3no(int a , int b , int c){
+
+        int sum = a+b+c;
+        return sum/3;
+
+    }
+
+    public static boolean isEvan (int a){
+        
+    }
+
+
+    public static void main(String[] args) {
+        
+        // average off 3 number !1
+
+        // System.out.println(Avg3no(4,5 ,7));
+
+
+
+    }
+
+}
--- functionMethods.java
+++ functionMethods.java
@@ -66,12 +66,23 @@ public class functionMethods {
 
         for ( int i = 2 ; i <= num ; i++){
             if(isPrime(i)){
-                System.out.println(i);
+                System.out.println(isPrime(i));
+                System.out.print(i+" ");
             }
         }
-
+        System.out.println("");
     }
 
+
+    public static double DecToBin(int num){
+
+        double binNum = 0;
+        for (int i = 0 ; i <= num ; i++ ){
+            binNum = binNum+((num%2)*Math.pow(10, i));
+            num = num/10;
+        }
+        return binNum;
+    }
 
 
     public static void main(String[] args) {
@@ -99,8 +110,15 @@ public class functionMethods {
         // System.out.println(BioCof(2,5));
 
         // print prime number !!
-        countPrime(100);
+        // countPrime(100);
 
+        // converting binary to dacimal 
+
+        // convert a number decimal to binary
+
+        System.out.println(DecToBin(5));
+
+        //
 
 
 
