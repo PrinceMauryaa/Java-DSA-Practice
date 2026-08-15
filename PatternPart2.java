a5ca7658767c50b584acfecf658dc1a31a884ab2
Prince Maurya <Prince.24vns@gmail.com>
2026-08-16 02:44:30 +0530

    pattern wala

---
+++ PatternPart2.java
@@ -0,0 +1,27 @@
+public class PatternPart2 {
+
+
+    public static void HollowBox(int a){
+
+        for(int i = 1 ; i <= a ; i ++){
+            for(int j = 1 ; j <= a ; j ++){
+                if(i == 1 || i == a || j == 1 || j == a){
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        HollowBox(5);
+        
+    }
+}
