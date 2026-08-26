b71190bdc8ad346c739be5de1e11f1c648c827b6
Prince Maurya <Prince.24vns@gmail.com>
2026-08-26 20:16:01 +0530

    bubble Sort !!

---
+++ BubbleSorting.java
@@ -0,0 +1,32 @@
+public class BubbleSorting {
+
+
+    public static void sort(int arr[]){ 
+
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = i+1 ; j<arr.length ; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+
+        for(int i = 0; i < arr.length ; i++ ){
+            System.out.println(arr[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {12,4,6,78,7,3,8,9};
+        int Num[] = {99,1,56,37,39,55,36,90,28,45,27,19};
+
+
+        sort(Num);
+
+
+    }
+}
